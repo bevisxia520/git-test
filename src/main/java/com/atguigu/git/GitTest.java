0be274722f94c68ad13f2a8ff5fc680e6c332f70
bevisxia520 <77184108+bevisxia520@users.noreply.github.com>
2022-02-28 22:15:08 +0800

    github update

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,6 +14,7 @@ public class GitTest {
         System.out.println("hot-fix test!");
         System.out.println("push test!");
         System.out.println("pull  test!");
-
+        
+        辟邪剑谱果然厉害
     }
 }

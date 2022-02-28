df66ed7395b73875218ffaea639a708a2dcf4df8
bevisxia520 <1206691711@qq.com>
2022-02-28 16:44:54 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author bevisxia
+ * @create 2022-02-28 4:35 下午
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
